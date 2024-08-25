deb831cae19de47cd9c5d8081a7c7923cb73e465
Maxim Leo <mxsleo@outlook.com>
2024-08-25 23:15:58 +0300

    Revert "Switch to external libs"
    This reverts commit 082814292cdb8fcf9600f98231d1debd7854d7f8.

--- Android.mk
+++ Android.mk
@@ -11,6 +11,32 @@ AROMA_CN := Flamboyan
 ## TARGET PATH
 AROMA_TARGET_PATH := $(PRODUCT_OUT)/aroma
 
+## ZLIB SOURCE FILES
+LOCAL_SRC_FILES := \
+    libs/zlib/adler32.c \
+    libs/zlib/crc32.c \
+    libs/zlib/infback.c \
+    libs/zlib/inffast.c \
+    libs/zlib/inflate.c \
+    libs/zlib/inftrees.c \
+    libs/zlib/zutil.c
+
+## PNG SOURCE FILES
+LOCAL_SRC_FILES += \
+    libs/png/png.c \
+    libs/png/pngerror.c \
+    libs/png/pnggccrd.c \
+    libs/png/pngget.c \
+    libs/png/pngmem.c \
+    libs/png/pngpread.c \
+    libs/png/pngread.c \
+    libs/png/pngrio.c \
+    libs/png/pngrtran.c \
+    libs/png/pngrutil.c \
+    libs/png/pngset.c \
+    libs/png/pngtrans.c \
+    libs/png/pngvcrd.c
+
 ## MINUTF8 & MINZIP SOURCE FILES
 LOCAL_SRC_FILES += \
     libs/minutf8/minutf8.c \
@@ -19,6 +45,29 @@ LOCAL_SRC_FILES += \
     libs/minzip/Inlines.c \
     libs/minzip/SysUtil.c \
     libs/minzip/Zip.c
+
+## FREETYPE SOURCE FILES
+LOCAL_SRC_FILES += \
+    libs/freetype/autofit/autofit.c \
+    libs/freetype/base/basepic.c \
+    libs/freetype/base/ftapi.c \
+    libs/freetype/base/ftbase.c \
+    libs/freetype/base/ftbbox.c \
+    libs/freetype/base/ftbitmap.c \
+    libs/freetype/base/ftglyph.c \
+    libs/freetype/base/ftinit.c \
+    libs/freetype/base/ftpic.c \
+    libs/freetype/base/ftstroke.c \
+    libs/freetype/base/ftsynth.c \
+    libs/freetype/base/ftsystem.c \
+    libs/freetype/cff/cff.c \
+    libs/freetype/pshinter/pshinter.c \
+    libs/freetype/psnames/psnames.c \
+    libs/freetype/raster/raster.c \
+    libs/freetype/sfnt/sfnt.c \
+    libs/freetype/smooth/smooth.c \
+    libs/freetype/truetype/truetype.c \
+    libs/freetype/base/ftlcdfil.c
 
 ## EDIFY PARSER SOURCE FILES
 LOCAL_SRC_FILES += \
@@ -63,11 +112,7 @@ LOCAL_MODULE := aroma_installer
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 ## INCLUDES & OUTPUT PATH
-LOCAL_C_INCLUDES := \
-    $(LOCAL_PATH)/include \
-    external/png \
-    external/zlib \
-    external/freetype/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 LOCAL_MODULE_PATH := $(AROMA_TARGET_PATH)
 
 ## COMPILER FLAGS
@@ -87,7 +132,7 @@ LOCAL_CFLAGS += -DAROMA_BUILD="\"$(AROMA_BUILD)\""
 LOCAL_CFLAGS += -DAROMA_BUILD_CN="\"$(AROMA_CN)\""
 
 ## INCLUDED LIBRARIES
-LOCAL_STATIC_LIBRARIES := libpng libc libz libft2_aroma_static libm
+LOCAL_STATIC_LIBRARIES := libm libc
 
 ifeq ($(MAKECMDGOALS),$(LOCAL_MODULE))
     $(shell rm -rf $(PRODUCT_OUT)/obj/EXECUTABLES/$(LOCAL_MODULE)_intermediates)
@@ -99,9 +144,6 @@ ifeq ($(MAKECMDGOALS),$(LOCAL_MODULE))
 endif
 
 include $(BUILD_EXECUTABLE)
-
-# freetype
-include $(LOCAL_PATH)/libs/freetype/Android.mk
 
 include $(CLEAR_VARS)
 
