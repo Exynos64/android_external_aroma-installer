f22b9c973e7db0fc69954a7c86473ba66a7d2537
Maxim Leo <mxsleo@outlook.com>
2024-08-25 23:31:48 +0300

    pngset.c: The compiler is not quiet anymore

--- libs/png/pngset.c
+++ libs/png/pngset.c
@@ -1174,7 +1174,6 @@ png_set_asm_flags (png_structp png_ptr, png_uint_32 asm_flags)
 /* Obsolete as of libpng-1.2.20 and will be removed from libpng-1.4.0 */
     if (png_ptr != NULL)
     png_ptr->asm_flags = 0;
-    asm_flags = asm_flags; /* Quiet the compiler */
 }
 
 /* This function was added to libpng 1.2.0 */
@@ -1186,9 +1185,6 @@ png_set_mmx_thresholds (png_structp png_ptr,
 /* Obsolete as of libpng-1.2.20 and will be removed from libpng-1.4.0 */
     if (png_ptr == NULL)
        return;
-    /* Quiet the compiler */
-    mmx_bitdepth_threshold = mmx_bitdepth_threshold;
-    mmx_rowbytes_threshold = mmx_rowbytes_threshold;
 }
 #endif /* ?PNG_ASSEMBLER_CODE_SUPPORTED */
 
