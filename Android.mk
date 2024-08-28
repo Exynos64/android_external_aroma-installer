c2f8dac52b73f37256b93bd0f9914f33fba21db8
Maxim Leo <mxsleo@outlook.com>
2024-08-28 14:04:39 +0300

    Android.mk: Move the build to the output product directory
    Source directories are now read-only.

--- Android.mk
+++ Android.mk
@@ -7,8 +7,9 @@ AROMA_VERSION := 3.00b1
 AROMA_BUILD := $(shell date +%y%m%d%H)
 AROMA_CN := Flamboyan
 
-## LOCAL PATH COPY
-AROMA_INSTALLER_LOCALPATH := $(LOCAL_PATH)
+## LOCAL AND TARGET PATHS
+AROMA_INSTALLER_LOCAL_PATH := $(LOCAL_PATH)
+AROMA_INSTALLER_TARGET_PATH := $(PRODUCT_OUT)/aroma
 
 ## MINUTF8 SOURCE FILE
 LOCAL_SRC_FILES += \
@@ -59,12 +60,12 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 
 ## INCLUDES & OUTPUT PATH
 LOCAL_C_INCLUDES := \
-    $(AROMA_INSTALLER_LOCALPATH)/include \
-    $(AROMA_INSTALLER_LOCALPATH)/libs \
+    $(AROMA_INSTALLER_LOCAL_PATH)/include \
+    $(AROMA_INSTALLER_LOCAL_PATH)/libs \
     external/png \
     external/zlib \
     external/freetype/include
-LOCAL_MODULE_PATH := $(AROMA_INSTALLER_LOCALPATH)/out
+LOCAL_MODULE_PATH := $(AROMA_INSTALLER_TARGET_PATH)
 
 ## COMPILER FLAGS
 LOCAL_CFLAGS := -O2 
@@ -95,21 +96,25 @@ endif
 include $(BUILD_EXECUTABLE)
 
 # freetype
-include $(AROMA_INSTALLER_LOCALPATH)/libs/freetype/Android.mk
+include $(AROMA_INSTALLER_LOCAL_PATH)/libs/freetype/Android.mk
 
 # minzip
-include $(AROMA_INSTALLER_LOCALPATH)/libs/minzip/Android.mk
+include $(AROMA_INSTALLER_LOCAL_PATH)/libs/minzip/Android.mk
 
 include $(CLEAR_VARS)
 
-AROMA_ZIP_TARGET := $(AROMA_INSTALLER_LOCALPATH)/out/aroma.zip
+AROMA_ZIP_TARGET := $(AROMA_INSTALLER_TARGET_PATH)/aroma.zip
 $(AROMA_ZIP_TARGET):
 	@echo "----- Making aroma zip installer ------"
-	$(hide) rm -rf $(AROMA_INSTALLER_LOCALPATH)/out/aroma.zip
-	$(hide) rm -rf $(AROMA_INSTALLER_LOCALPATH)/assets/META-INF/com/google/android/update-binary
-	$(hide) cp $(AROMA_INSTALLER_LOCALPATH)/out/aroma_installer $(AROMA_INSTALLER_LOCALPATH)/assets/META-INF/com/google/android/update-binary
-	$(hide) cd $(AROMA_INSTALLER_LOCALPATH)/assets && zip -r9 ../out/aroma.zip .
-	$(hide) rm -rf $(AROMA_INSTALLER_LOCALPATH)/assets/META-INF/com/google/android/update-binary
+	$(hide) rm -rf $(AROMA_INSTALLER_TARGET_PATH)/assets \
+	               $(AROMA_INSTALLER_TARGET_PATH)/aroma.zip
+	$(hide) cp -R $(AROMA_INSTALLER_LOCAL_PATH)/assets \
+	              $(AROMA_INSTALLER_TARGET_PATH)/assets
+	$(hide) cp $(AROMA_INSTALLER_TARGET_PATH)/aroma_installer \
+	           $(AROMA_INSTALLER_TARGET_PATH)/assets/META-INF/com/google/android/update-binary
+	$(hide) pushd $(AROMA_INSTALLER_TARGET_PATH)/assets && \
+	        zip -r9 ../aroma.zip . && \
+	        popd
 	@echo "Made flashable aroma.zip: $@"
 
 .PHONY: aroma_installer_zip
